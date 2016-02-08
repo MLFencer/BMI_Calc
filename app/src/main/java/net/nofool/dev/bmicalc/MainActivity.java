e4fc96e6279fae86025ef1197d499af7628bab82
Macgyver01 <Macgyver01@users.noreply.github.com>
2016-02-08 09:12:56 -0500

    Finished, Ready for submission.

--- app/src/main/java/net/nofool/dev/bmicalc/MainActivity.java
+++ app/src/main/java/net/nofool/dev/bmicalc/MainActivity.java
@@ -2,11 +2,14 @@ package net.nofool.dev.bmicalc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private String con;
     private Double resultNum;
     private String result;
+    DecimalFormat df = new DecimalFormat("#.##");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +37,11 @@ public class MainActivity extends AppCompatActivity {
         hightFTEdit = (EditText)findViewById(R.id.hightEditText);
         hightINEdit = (EditText)findViewById(R.id.inchEditText);
         resultText = (TextView)findViewById(R.id.resultTextView);
+        weightEdit.setHint("Pounds");
+        hightFTEdit.setHint("Feet");
+        hightINEdit.setHint("Inches");
         unitSwitch = (Switch)findViewById(R.id.unitSwitch);
+        unitSwitch.setText("Imperial");
         unitSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
                 if (isChecked == true){
@@ -55,28 +63,66 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         calcButton = (Button)findViewById(R.id.calcButton);
+        calcButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage();
+            }
+        });
     }
     public void sendMessage(){
         if (mode == 0){
-            con = hightFTEdit.getText().toString();
-            hightFT= Double.parseDouble(con);
-            con = hightINEdit.getText().toString();
-            hightIN = Double.parseDouble(con);
+            if (hightFTEdit.getText().length()!=0) {
+                con = hightFTEdit.getText().toString();
+                hightFT = Double.parseDouble(con);
+            } else{
+                hightFT=0.0;
+            }
+            if (hightINEdit.getText().length()!=0) {
+                con = hightINEdit.getText().toString();
+                hightIN = Double.parseDouble(con);
+            } else {
+                hightIN=0.0;
+            }
             hightIN=hightFT*12 + hightIN;
-            con = weightEdit.getText().toString();
-            weight = Double.parseDouble(con);
+            if (weightEdit.getText().length()!=0) {
+                con = weightEdit.getText().toString();
+                weight = Double.parseDouble(con);
+            } else {
+                weight = 0.0;
+            }
             resultNum =703*(weight/(hightIN*hightIN));
-            resultText.setText("Your BMI is:"+resultNum);
+            if (Double.isNaN(resultNum)){
+                resultText.setText("Please enter numbers in the fields provided!");
+            } else {
+                resultText.setText("Your BMI is:" + df.format(resultNum));
+            }
         } else if (mode ==1){
-            con = hightFTEdit.getText().toString();
-            hightFT= Double.parseDouble(con);
-            con = hightINEdit.getText().toString();
-            hightIN = Double.parseDouble(con);
-            hightFT=hightFT + (hightIN*.01);
-            con = weightEdit.getText().toString();
-            weight = Double.parseDouble(con);
-            resultNum = (weight/(hightIN*hightIN));
-            resultText.setText("Your BMI is:"+resultNum);
+           if (hightFTEdit.getText().length()!=0) {
+             con = hightFTEdit.getText().toString();
+             hightFT= Double.parseDouble(con);
+            } else{
+                hightFT=0.0;
+            }
+            if (hightINEdit.getText().length()!=0) {
+                con = hightINEdit.getText().toString();
+                hightIN = Double.parseDouble(con);
+            } else {
+                hightIN=0.0;
+            }
+            if (weightEdit.getText().length()!=0) {
+                con = weightEdit.getText().toString();
+                weight = Double.parseDouble(con);
+            } else {
+                weight = 0.0;
+            }
+            hightFT = (hightFT +(hightIN/100));
+            resultNum = (weight/(hightFT*hightFT));
+            if (Double.isNaN(resultNum)){
+                resultText.setText("Please enter numbers in the fields provided!");
+            } else {
+                resultText.setText("Your BMI is:" + df.format(resultNum));
+            }
         }
     }
 }
